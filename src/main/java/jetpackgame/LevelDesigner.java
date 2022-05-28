dc03b3e8d7bca0f4ac8561baeedd84b1f342aafa
brodderickrodriguez <bcr@brodderick.com>
2022-05-27 18:48:10 -0600

    working on level builder

--- src/main/java/jetpackgame/LevelDesigner.java
+++ src/main/java/jetpackgame/LevelDesigner.java
@@ -1,18 +1,49 @@
 package main.java.jetpackgame;
 
 import java.awt.*;
+import java.awt.event.*;
 
 
-public class LevelDesigner extends ContentPane  {
+enum ModState {
+    ADD_PLATFORM,
+    ADD_PLAYER,
+    ADD_PORTAL,
+    ADD_ENEMY,
+    ADD_GAS_CAN;
+
+    static ModState getModStateFromKeyCode(int keyCode) {
+        ModState state;
+
+        switch (keyCode) {
+            case KeyEvent.VK_P -> state = ModState.ADD_PLATFORM;
+            case KeyEvent.VK_M -> state = ModState.ADD_PLAYER;
+            case KeyEvent.VK_O -> state = ModState.ADD_PORTAL;
+            case KeyEvent.VK_E -> state = ModState.ADD_ENEMY;
+            case KeyEvent.VK_G -> state = ModState.ADD_GAS_CAN;
+            default -> state = null;
+        }
+
+        return state;
+    }
+}
+
+public class LevelDesigner extends ContentPane implements MouseListener, MouseMotionListener {
+    private ModState modState = ModState.ADD_PLATFORM;
+    private Sprite currentSprite = null;
+    private double zoomDelta;
+
     @Override
     public void init() {
         WorldBorder.makeBorders();
         SpriteManager.update();
 
-        double zoomDelta = this.getZoomOutDelta();
+        this.zoomDelta = this.getZoomOutDelta();
         this.zoomOut(zoomDelta);
         this.centerContents();
         this.repaint();
+        this.addMouseListener(this);
+        this.addMouseMotionListener(this);
+        Controller.getCurrentController().setKeyPressedCallBack(this::keyEvent);
     }
 
     double getZoomOutDelta() {
@@ -20,24 +51,88 @@ public class LevelDesigner extends ContentPane  {
         return (double)Const.WINDOW_HEIGHT / denom;
     }
 
-    void updateComponentSize(Component c, double delta) {
+    void updateComponentSize(Component c) {
         Rectangle bounds = c.getBounds();
-        bounds.x *= delta;
-        bounds.y *= delta;
-        bounds.width *= delta;
-        bounds.height *= delta;
+        bounds.x *= this.zoomDelta;
+        bounds.y *= this.zoomDelta;
+        bounds.width *= this.zoomDelta;
+        bounds.height *= this.zoomDelta;
         c.setBounds(bounds);
     }
 
     void zoomOut(double delta) {
         for (Sprite sprite: SpriteManager.getEverySprite()) {
-            this.updateComponentSize(sprite, delta);
+            this.updateComponentSize(sprite);
 
             for (Component c: sprite.getComponents()) {
-                this.updateComponentSize(c, delta);
+                this.updateComponentSize(c);
             }
 
         }
     }
 
+    Sprite getSpriteForCurrentModState(int x, int y) {
+        Sprite result = null;
+        switch (this.modState) {
+            case ADD_PLATFORM -> result = new Platform(x, y);
+            case ADD_PLAYER -> result = new Player(x, y);
+//            case ADD_PORTAL -> result = new Portal(x, y, x, y);
+            case ADD_ENEMY -> result = new Enemy(x, y);
+            case ADD_GAS_CAN -> result = new GasCan(x, y);
+        }
+
+        return result;
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        int x = (int)(e.getX() * (1 / this.zoomDelta));
+        int y = (int)(e.getY() * (1 / this.zoomDelta));
+        this.currentSprite = this.getSpriteForCurrentModState(x, y);
+        this.currentSprite.setXY(e.getX(), e.getY());
+        System.out.println(this.currentSprite.getBounds());
+
+        if (this.currentSprite == null) {
+            return;
+        }
+
+        this.updateComponentSize(this.currentSprite);
+        this.add(this.currentSprite);
+        this.repaint();
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        if (this.currentSprite == null) {
+            return;
+        }
+
+        this.currentSprite.setXY(e.getX(), e.getY());
+        this.repaint();
+    }
+
+    public Void keyEvent(int keyCode) {
+        ModState newState = ModState.getModStateFromKeyCode(keyCode);
+
+        if (newState != null) {
+            this.modState = newState;
+            System.out.println("setting mod state to: " + this.modState);
+        }
+        return null;
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) { }
+
+    @Override
+    public void mouseClicked(MouseEvent e) { }
+
+    @Override
+    public void mouseEntered(MouseEvent e) { }
+
+    @Override
+    public void mouseExited(MouseEvent e) { }
+
+    @Override
+    public void mouseMoved(MouseEvent e) { }
 }
